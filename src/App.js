5f7d749e0b7e7e168a530d3166cab80ea639e70b
Mpt-proyect <ProyectoMpt@gmail.com>
2020-07-16 20:37:33 -0400

    login

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Logo_mpt from './components/Logo_mpt';
 import Inicio_mpt from './components/Inicio_mpt';
 import Contenido from './components/Contenido';
 import Items from './components/Items';
-
+import Login from './Login/Login';
 
 function App() {
   return (
     <div className="App">
      
       <Navigation></Navigation>
+      <Login></Login>
       <Contenido></Contenido>
       <Inicio_mpt></Inicio_mpt>
       <Items></Items>
---
+++ src/Login/Firebase.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyA0kAD0tXmna0WYG7o4EwKqcykqWXuRdaU",
+    authDomain: "proyecto-mpt---usuarios.firebaseapp.com",
+    databaseURL: "https://proyecto-mpt---usuarios.firebaseio.com",
+    projectId: "proyecto-mpt---usuarios",
+    storageBucket: "proyecto-mpt---usuarios.appspot.com",
+    messagingSenderId: "948056815403",
+    appId: "1:948056815403:web:98a64f1572c493bbc0776e",
+    measurementId: "G-3NSHJR7RR2"
+}
